68f0fadcd3ce6f4bb29b1b60ca98e4cf47319a20
Brandon Leeds <brandonleeds@gmail.com>
2018-03-20 01:04:47 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,13 @@
+class PostValidator < ActiveModel::Validator
+  def validate(record)
+    record.title.include?("Won't Believe") || record.title.include?("Secret") || record.title.include?("Top [number]") || record.title.include?("Guess")
+  end
+end
+
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validates_with PostValidator
-end
-
-class PostValidator < ActiveModel::Validator
-  def validate(record)
-    record.title.include?("Won't Believe") || record.title.include?("Secret") || record.title.include?("Top [number]") || record.title.include?("Guess")
-  end
 end
