0845d0acb9c7584d48a74443ff18e6a0c1b5286e
Brandon Leeds <brandonleeds@gmail.com>
2018-03-20 01:07:17 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class PostValidator < ActiveModel::Validator
   def validate(record)
-    !!record.title.to_sempty?
+    !!record.title.to_s.empty?
     record.title.include?("Won't Believe") || record.title.include?("Secret") || record.title.include?("Top [number]") || record.title.include?("Guess")
   end
 end
